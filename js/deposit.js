7eebbd61680f33205a0d6201fc0fe032730c7789
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-08-15 19:43:01 +0600

    Update deposit and balance

--- js/deposit.js
+++ js/deposit.js
@@ -8,6 +8,10 @@
  * 4. get the previous deposit total
  * 
  * 5. calculate new deposit total and save the value to the deposit total
+ * 
+ * 6. get current balance 
+ * 
+ * 7. calculate the new balance and set it to the balance total element
  */
 
 // step-1:
@@ -29,4 +33,15 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     // step-5:
     const newDepositTotal = previousDepositTotal + newDepositAmount;
     depositTotalElement.innerText = newDepositTotal;
+
+    // step-6: 
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+
+    // step-7:
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
 })
---
+++ js/withdraw.js
@@ -0,0 +1,11 @@
+/* 
+1. add event handler with the withdraw button
+2. 
+
+
+
+*/
+
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    console.log('sssss');
+})
